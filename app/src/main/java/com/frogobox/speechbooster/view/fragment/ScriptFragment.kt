80ff91036ad6ae48296bd331be31abb3170040b1
amirisback <faisalamircs@gmail.com>
2019-08-26 01:29:16 +0700

    add: content fragment Script

--- app/src/main/java/com/frogobox/speechbooster/view/fragment/ScriptFragment.kt
+++ app/src/main/java/com/frogobox/speechbooster/view/fragment/ScriptFragment.kt
@@ -5,12 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 import com.frogobox.speechbooster.R
 import com.frogobox.speechbooster.base.BaseFragment
+import com.frogobox.speechbooster.base.BaseListener
+import com.frogobox.speechbooster.helper.ConstHelper
 import com.frogobox.speechbooster.model.Script
+import com.frogobox.speechbooster.navigation.Navigation
+import com.frogobox.speechbooster.navigation.Navigation.createBundle.baseCreateBundleObject
+import com.frogobox.speechbooster.navigation.Route
+import com.frogobox.speechbooster.navigation.Route.routeImplicit.openScriptDetailActivity
+import com.frogobox.speechbooster.view.viewadapter.adapter.ScriptAdapter
+import kotlinx.android.synthetic.main.activity_script.*
 
-class ScriptFragment : BaseFragment() {
+class ScriptFragment : BaseFragment(), BaseListener<Script> {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -20,7 +29,44 @@ class ScriptFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupRecyclerView()
 
     }
+
+    private fun setupRecyclerView(){
+        val noteList = mutableListOf<Script>()
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummyLong)))
+        noteList.add(Script(getString(R.string.dummy), getString(R.string.dummy)))
+
+        val adapter = ScriptAdapter()
+        context?.let { adapter.setLayoutItem(it, R.layout.recyclerview_item_notes) }
+        adapter.setListener(this)
+        adapter.setRecyclerViewData(noteList)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+    }
+
+    override fun onItemClicked(data: Script) {
+        context?.let {
+            openScriptDetailActivity(it,baseCreateBundleObject(ConstHelper.Extra.EXTRA_NOTE, data))
+        }
+    }
+
+    override fun onItemLongClicked(data: Script) {
+
+    }
+
+
 }
